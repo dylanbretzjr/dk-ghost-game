8896beba3cd0e05f8d194a7f752353f26508bbec
Dylan Bretz Jr. <118217492+dylanbretzjr@users.noreply.github.com>
2025-10-06 13:49:14 -0400

    Update dk-ghost-game.py

--- dk-ghost-game.py
+++ dk-ghost-game.py
@@ -2,27 +2,26 @@
 
 from random import randint
 
-print("Ghost Game")
+play_game = True
 
-play_again = "Y"
-
-while play_again.upper() == "Y":
+while play_game:
+    print("Ghost Game")
     feeling_brave = True
     score = 0
-
+    
     while feeling_brave:
         ghost_door = randint(1, 3)
         print("Three doors ahead ...")
         print("A ghost behind one.")
         print("Which door do you open?")
         door = input("1, 2, or 3? ")
-
+        
         if door not in ["1", "2", "3"]:
             print("Oops! That's not a choice.")
             continue
-
+        
         door_num = int(door)
-
+        
         if door_num == ghost_door:
             print("GHOST!")
             feeling_brave = False
@@ -30,9 +29,18 @@ while play_again.upper() == "Y":
             print("No ghost!")
             print("You enter the next room.")
             score += 1
-
+            
     print("Run away!")
     print("Game over! You scored", score)
-    play_again = input("Do you want to play again? (Y or N) ")
 
+    replay = input("Do you want to play again? (Y/N) ").upper()
+
+    if replay == "Y":
+        play_game = True
+    if replay == "N":
+        play_game = False
+    elif replay != "Y":
+        print("Invalid input. Assuming you don't want to play.")
+        play_game = False
+    
 print("Goodbye!")
