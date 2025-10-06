0c875c6f25b555506b14d70c10f8dce384a13617
Dylan Bretz Jr. <118217492+dylanbretzjr@users.noreply.github.com>
2025-10-06 13:41:56 -0400

    Update dk-ghost-game.py

--- dk-ghost-game.py
+++ dk-ghost-game.py
@@ -1,8 +1,12 @@
 # Ghost Game
+
 from random import randint
+
 print("Ghost Game")
+
 feeling_brave = True
 score = 0
+
 while feeling_brave:
     ghost_door = randint(1, 3)
     print("Three doors ahead ...")
@@ -10,12 +14,16 @@ while feeling_brave:
     print("Which door do you open?")
     door = input("1, 2, or 3? ")
     door_num = int(door)
-    if door_num == ghost_door:
-        print("GHOST!")
-        feeling_brave = False
+    if door_num == 1 or door_num == 2 or door_num == 3:
+        if door_num == ghost_door:
+            print("GHOST!")
+            feeling_brave = False
+        else:
+            print("No ghost!")
+            print("You enter the next room.")
+            score = score + 1
     else:
-        print("No ghost!")
-        print("You enter the next room.")
-        score = score + 1
+        print("Oops! That's not a choice.")
+
 print("Run away!")
 print("Game over! You scored", score)
